f0d65d3c482b409bc1b2743b77c349cfcf797a95
beasty <github@beastyrabbit.de>
2026-02-26 17:10:11 +0100

    chore: align game barrel export for PR #14

--- lib/game/index.ts
+++ lib/game/index.ts
@@ -47,5 +47,7 @@ export * from "./survival";
 // House build prerequisite planner
 export * from "./housePlanner";
 
-// Colony achievement tracking
+// Cat gossip network (rumor propagation)
+export * from "./gossipNetwork";
+
 export * from "./achievements";
