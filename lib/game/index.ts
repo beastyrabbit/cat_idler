e4e1366f609d8aba6be08bbba1945fe93ca032e9
beasty <github@beastyrabbit.de>
2026-02-26 17:10:17 +0100

    chore: align game barrel export for PR #10

--- lib/game/index.ts
+++ lib/game/index.ts
@@ -47,5 +47,7 @@ export * from "./survival";
 // House build prerequisite planner
 export * from "./housePlanner";
 
-// Seasonal cycle system
+// Cat gossip network (rumor propagation)
+export * from "./gossipNetwork";
+
 export * from "./seasons";
